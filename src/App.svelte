38ae8ff8d4479f68d068578c75ecbc0cfa698560
cindyeme <emereninicynthia@gmail.com>
2022-08-20 22:41:18 +0100

    added limit

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,7 @@
 	let allOperations = [];
 
 	(async () => {
-		const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+		const response = await fetch("https://jsonplaceholder.typicode.com/photos?_limit=100");
 		const posts = await response.json();
 		items = posts;
 	})();
